1e8741b6d2b36ba7084aff61f155f3193f6fa6b6
nahasonnyaga <e35-5321-2020@student.mksu.ac.ke>
2025-06-29 12:52:52 +0300

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -1,8 +1,56 @@
 // js/utils.js
-// Linkify hashtags and mentions
+
+/**
+ * Linkify hashtags, mentions, and URLs in content.
+ * - Mentions (@user) ➔ <a href="profile.html?username=user" class="mention">@user</a>
+ * - Hashtags (#topic) ➔ <a href="index.html?hashtag=topic" class="hashtag">#topic</a>
+ * - URLs ➔ <a href="..." class="external-link">...</a>
+ * Escapes HTML to prevent XSS.
+ * @param {string} text
+ * @returns {string} HTML with links
+ */
 function linkifyContent(text) {
-  // Mentions
-  let withMentions = text.replace(/@(\w+)/g, '<a href="profile.html?username=$1" class="mention">@$1</a>');
-  // Hashtags
-  return withMentions.replace(/#(\w+)/g, '<a href="index.html?hashtag=$1" class="hashtag">#$1</a>');
+  if (!text) return "";
+
+  // Escape HTML to prevent XSS
+  function escapeHTML(str) {
+    return str.replace(/[&<>"']/g, function (m) {
+      return ({
+        '&': '&amp;',
+        '<': '&lt;',
+        '>': '&gt;',
+        '"': '&quot;',
+        "'": '&#39;'
+      })[m];
+    });
+  }
+
+  // First escape any HTML in user input
+  let safe = escapeHTML(text);
+
+  // Linkify URLs (http(s):// or www.)
+  safe = safe.replace(
+    /(\bhttps?:\/\/[^\s<]+[^\s<\.\,\!\?\)\]\}])|(\bwww\.[^\s<]+[^\s<\.\,\!\?\)\]\}])/gi,
+    function (url) {
+      let fullUrl = url.startsWith('www.') ? 'http://' + url : url;
+      return `<a href="${fullUrl}" class="external-link" target="_blank" rel="noopener">${url}</a>`;
+    }
+  );
+
+  // Linkify mentions
+  safe = safe.replace(
+    /@(\w+)/g,
+    '<a href="profile.html?username=$1" class="mention">@$1</a>'
+  );
+
+  // Linkify hashtags
+  safe = safe.replace(
+    /#(\w+)/g,
+    '<a href="index.html?hashtag=$1" class="hashtag">#$1</a>'
+  );
+
+  return safe;
 }
+
+// Optionally: export for use in modules
+// export { linkifyContent };
